2f37449e3fd192c3a8b0fa1bd644d8061bd582db
Chris Nevin <chris.nevin@tigerspike.com>
2022-10-22 00:17:49 +1000

    Updated dependencies.

--- UserViewController/Package.swift
+++ UserViewController/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "../Stylesheet"),
         .package(path: "../UserPresenter"),
-        .package(name: "AutoLayoutBuilder", url: "https://github.com/cjnevin/AutoLayoutBuilder", from: "1.0.9")
+        .package(name: "AutoLayoutBuilder", url: "https://github.com/cjnevin/AutoLayoutBuilder", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
