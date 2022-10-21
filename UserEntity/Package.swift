0a4c735780db27006e85853bbbf3120bddf94f51
Chris Nevin <chris.nevin@tigerspike.com>
2022-10-22 01:08:49 +1000

    Updated packages.

--- UserEntity/Package.swift
+++ UserEntity/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "PhantomTypes", url: "https://github.com/cjnevin/PhantomTypes", from: "1.0.6"),
-        .package(name: "PropertyWrappers", url: "https://github.com/cjnevin/PropertyWrappers", from: "1.0.1")
+        .package(name: "PhantomTypes", url: "https://github.com/cjnevin/PhantomTypes", from: "1.0.7"),
+        .package(name: "PropertyWrappers", url: "https://github.com/cjnevin/PropertyWrappers", from: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
