bada65856cae32bf1c857e79b244e663ab574d99
Chris Nevin <chris.nevin@tigerspike.com>
2022-11-03 15:01:03 +1000

    Cleaned up unused code.

--- DependencyContainer/Sources/DependencyContainer/DependencyContainer.swift
+++ DependencyContainer/Sources/DependencyContainer/DependencyContainer.swift
@@ -3,34 +3,10 @@ public protocol DependencyKey {
     static var currentValue: Self.Value { get set }
 }
 
-public protocol LazyDependencyKey {
-    associatedtype Value
-    static var currentValue: Self.Value? { get set }
-}
-
-extension LazyDependencyKey {
-    static var value: Self.Value {
-        get {
-            guard let currentValue = currentValue else {
-                preconditionFailure("A value must be set before accessing the property.")
-            }
-            return currentValue
-        }
-        set {
-            currentValue = newValue
-        }
-    }
-}
-
 public class DependencyContainer {
     private static var current = DependencyContainer()
 
     public static subscript<K>(key: K.Type) -> K.Value where K: DependencyKey {
-        get { key.currentValue }
-        set { key.currentValue = newValue }
-    }
-
-    public static subscript<K>(key: K.Type) -> K.Value? where K: LazyDependencyKey {
         get { key.currentValue }
         set { key.currentValue = newValue }
     }
@@ -41,10 +17,6 @@ public class DependencyContainer {
     }
 
     public static func set<K>(initialValue: K.Value, key: K.Type) where K: DependencyKey {
-        key.currentValue = initialValue
-    }
-
-    public static func set<K>(initialValue: K.Value, key: K.Type) where K: LazyDependencyKey {
         key.currentValue = initialValue
     }
 }
