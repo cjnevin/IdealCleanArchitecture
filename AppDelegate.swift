f7cd3b840bf5085f7742b3dc8dc3c1b0263fc41f
Chris Nevin <cjnevin@protonmail.com>
2022-11-18 17:47:07 +1000

    Moved selection to app delegate.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         tabRouter = TabRouter(window: window)
         window?.makeKeyAndVisible()
+        tabRouter?.selectSettingsTab()
         return true
     }
 }
--- Modules/UI/Home/Routers/DefaultRouter+HomeRoute.swift
+++ Modules/UI/Home/Routers/DefaultRouter+HomeRoute.swift
@@ -11,7 +11,6 @@ public class TabRouter: DefaultRouter {
         root = window?.rootViewController
         startHome()
         startSettings()
-        selectSettingsTab()
     }
 }
 
