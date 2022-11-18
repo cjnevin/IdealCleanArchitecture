00a93c9e3cb13de5803d7cf14caebb95d68f5953
Chris Nevin <cjnevin@protonmail.com>
2022-11-18 20:33:06 +1000

    Removed dead folders.

--- Shared/Transitions/Transition.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-public protocol Transition: AnyObject {
-    var isAnimated: Bool { get set }
-    func open(_ viewController: UIViewController, from: UIViewController, completion: (() -> Void)?)
-    func close(_ viewController: UIViewController, completion: (() -> Void)?)
-}
