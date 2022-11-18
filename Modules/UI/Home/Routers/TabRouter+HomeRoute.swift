add616281bf29d8af8b2478f0758b491d693f7d0
Chris Nevin <cjnevin@protonmail.com>
2022-11-18 17:49:31 +1000

    Split logic.

--- Modules/UI/Home/Routers/TabRouter+HomeRoute.swift
+++ Modules/UI/Home/Routers/TabRouter+HomeRoute.swift
@@ -1,19 +1,6 @@
 import Core
 import UIKit
 
-public class TabRouter: DefaultRouter {
-    var homeRouter: DefaultRouter?
-    var settingsRouter: DefaultRouter?
-    
-    public init(window: UIWindow?) {
-        super.init(rootTransition: EmptyTransition())
-        window?.rootViewController = UITabBarController()
-        root = window?.rootViewController
-        startHome()
-        startSettings()
-    }
-}
-
 extension TabRouter: HomeRoute {
     public func startHome() {
         let transition = TabTransition(at: 0, name: "Home", icon: "house")
